0222a986c578aef6ee14cb36c4276157d71e70aa
James <james.kiesel@gmail.com>
2022-06-29 12:36:18 +1200

    Custom mailchimp form

--- components/EmailSubscribe.js
+++ components/EmailSubscribe.js
@@ -1,10 +1,21 @@
 import Mailchimp from 'react-mailchimp-subscribe'
 import styles from '../styles/EmailSubscribe.module.css'
+import useTranslation from 'next-translate/useTranslation'
 
 function EmailSubscribe() {
+  const { t } = useTranslation('common')
+
   return (
     <div className={styles.form}>
-      <Mailchimp url={process.env.NEXT_PUBLIC_MAILCHIMP_SUBSCRIBE_URL} />
+      <Mailchimp
+        url={process.env.NEXT_PUBLIC_MAILCHIMP_SUBSCRIBE_URL}
+        render={({ subscribe, status, message }) => (
+          <form>
+            <input type="email" placeholder={t('mailchimp.email')} />
+            <button>{t('mailchimp.subscribe')}</button>
+          </form>
+        )}
+      />
     </div>
   )
 }
