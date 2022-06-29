b50939b67bbc29eb854b8706f6cbeaaf5eaa4b2a
James <james.kiesel@gmail.com>
2022-06-29 15:50:16 +1200

    Satisfactory email signup

--- components/EmailSubscribe.js
+++ components/EmailSubscribe.js
@@ -1,23 +1,38 @@
 import Mailchimp from 'react-mailchimp-subscribe'
 import styles from '../styles/EmailSubscribe.module.css'
+import { useRef } from 'react'
 import useTranslation from 'next-translate/useTranslation'
+import ClipLoader from 'react-spinners/ClipLoader'
 
-function EmailSubscribe() {
+const renderEmailForm = (t, { email }) => (
+  ({ subscribe, status, message }) => (
+    <form onSubmit={(event) => {
+      event.preventDefault()
+      if (!email.current || !email.current.value) { return }
+
+      subscribe({ EMAIL: email.current.value })
+      event.target.reset()
+    }}>
+      <input disabled={status === 'sending'} ref={email} type="email" placeholder={t('mailchimp.email')} />
+      <button type="submit">
+        <span className={status === 'sending' ? styles.submit : null}>{t('mailchimp.subscribe')}</span>
+        <ClipLoader className={status === 'sending' ? styles.loader : styles.hidden} size={20} />
+      </button>
+      <p className={styles.message}>{message}</p>
+    </form>
+  )
+)
+
+export default function EmailSubscribe() {
+  const email = useRef()
   const { t } = useTranslation('common')
 
   return (
     <div className={styles.form}>
       <Mailchimp
         url={process.env.NEXT_PUBLIC_MAILCHIMP_SUBSCRIBE_URL}
-        render={({ subscribe, status, message }) => (
-          <form>
-            <input type="email" placeholder={t('mailchimp.email')} />
-            <button>{t('mailchimp.subscribe')}</button>
-          </form>
-        )}
+        render={renderEmailForm(t, { email })}
       />
     </div>
   )
 }
-
-export default EmailSubscribe
