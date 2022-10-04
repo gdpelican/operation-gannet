532fff3249b391d1ebe9275146257790674fa183
James <james.kiesel@gmail.com>
2022-10-04 22:51:23 +1300

    Parse notion info better

--- datasources/notion.js
+++ datasources/notion.js
@@ -1,4 +1,5 @@
 import { Client } from '@notionhq/client'
+import camelcase from 'camelcase'
 
 const client = new Client({ auth: process.env.NOTION_ACCESS_TOKEN })
 
@@ -6,5 +7,14 @@ export async function fetchProductions() {
   const productions = await client.databases.query({
     database_id: process.env.NOTION_PRODUCTIONS_DATABASE_ID
   })
-  return productions.results.map(production => production.properties)
+  return productions.results.map(extractProduction)
 }
+
+const extractProduction = ({ properties }) =>
+  Object.fromEntries(Object.entries(properties).map(extractValue))
+
+const extractValue = ([key, value]) =>
+  [
+    camelcase(key),
+    value.type === 'title' ? value[value.type][0]?.plain_text : value[value.type]
+  ]
